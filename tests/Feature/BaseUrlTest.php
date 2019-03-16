1a540e7f2df8c5c366c889aec70e808a3cbbaaac
youngtyron <gro-jorge@mail.ru>
2019-03-16 21:06:37 +0500

    Base url test

---
+++ tests/Feature/BaseUrlTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BaseUrlTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testFirstPage()
+    {
+    	$response = $this->get('/');
+        $response->assertStatus(200);
+    }
+    public function testLoginPage()
+    {
+    	$response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+    public function testRegisterPage()
+    {
+    	$response = $this->get('/register');
+        $response->assertStatus(200);
+    }
+    public function testGamePageAnonymous(){
+    	$response = $this->get('/game');
+    	$response->assertRedirect('login');
+    }
+}
